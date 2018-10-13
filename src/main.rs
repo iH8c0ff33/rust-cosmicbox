84d3ca2c6fc692d97d962c70757c66154efd4571
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2018-10-13 17:34:16 +0200

    Add example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate cosmicbox;
+extern crate hidapi;
+
+use cosmicbox::{CosmicBox, GenericCosmicBox};
+use hidapi::HidApi;
+
 fn main() {
-    println!("Hello, world!");
+    let hid = HidApi::new().unwrap();
+    let cb = CosmicBox::connect(hid);
+    cb.reset();
+
+    println!("Cosmic Box reset done");
 }
