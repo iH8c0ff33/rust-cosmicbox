0a2c8952de5f39bb121fb9888ac3a9bfd063efa8
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2018-10-13 17:34:10 +0200

    Add connection helper

--- src/hid/mod.rs
+++ src/hid/mod.rs
@@ -1,3 +1,27 @@
+use hidapi::{HidApi, HidDevice};
+
+use {CosmicBox, PRODUCT_ID, VENDOR_ID};
+
 pub mod logic;
 pub mod packet;
 pub mod proto;
+
+impl CosmicBox<HidDevice> {
+    pub fn connect(hid: HidApi) -> Self {
+        Self {
+            device: hid.open(VENDOR_ID, PRODUCT_ID).unwrap(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use hidapi::HidApi;
+
+    use CosmicBox;
+
+    #[test]
+    fn cosmicbox_connect() {
+        let _ = CosmicBox::connect(HidApi::new().unwrap());
+    }
+}
